bf692b222cea3390174ca7a2bc8100559740f930
Sara Hassan <shassan2016@gmail.com>
2023-08-30 14:35:59 -0600

    Cat and Dog classes were added to Animal package

---
+++ src/Animal/Cat.java
@@ -0,0 +1,10 @@
+package Animal;
+
+public class Cat {
+    String name;
+    int age;
+
+    public Cat() {
+        System.out.println("Cat is crated");
+    }
+}
---
+++ src/Animal/Dog.java
@@ -0,0 +1,10 @@
+package Animal;
+
+public class Dog {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Dog Barks");
+            
+        }
+    }
+}
